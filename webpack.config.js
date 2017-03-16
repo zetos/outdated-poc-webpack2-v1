7d84c3d650ae9c5277604c9749bcfa82bf81512b
Zetos Lima <cesar_manoel@hotmail.com>
2017-03-15 23:51:03 -0300

    webpack server added

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var path = require('path');
 
 module.exports = {
     entry: './src/app.js',
     output: {
-        path: 'dist',
+        path: path.resolve(__dirname, 'dist'),
         filename: 'app.bundle.js'
     },
     module: {
